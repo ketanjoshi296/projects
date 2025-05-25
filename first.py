a78434eea5eb586da25ae600a0342ce5ffa2ef49
Ketan <home@Sakshis-MacBook-Pro.local>
2025-05-25 15:08:42 -0700

    setting up vs code env for this repo

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
 #This is first file commit to my git-repo. Don't judge.
 
 print("Hello! We will be creating some random projects in this repo.")
-
+print("This is a second line written from VS code. Setting it up!")
